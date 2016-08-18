b7ea5b7c8d679bcd47677f1f0408ea7b6d39d5a0
Photon_Horizon <Photon_Horizon>
2016-08-18 13:18:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/photon_horizon/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.photon_horizon.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
